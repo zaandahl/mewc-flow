a7826eeecd70981725d0aabcc630fcea4a727672
zaandahl <zaandahl@users.noreply.github.com>
2024-07-26 17:53:29 +1000

    removing lib_data.py and adding new lib_common.py

--- src/lib_common.py
+++ src/lib_common.py
@@ -1,5 +1,71 @@
-import yaml
+import logging, os, re, warnings, yaml
+from jax import devices
+from keras import distribution
 
+# Configure logging to suppress warnings and errors
+def configure_logging():
+    """
+    Set the environment variable to allow INFO messages but suppress WARNING messages.
+    """
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress WARNING messages and ERROR logs
+    logging.getLogger('tensorflow').setLevel(logging.ERROR)
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    warnings.simplefilter(action='ignore', category=Warning)
+    warnings.filterwarnings("ignore")
+
+# Reads a yaml file and returns a dictionary
 def read_yaml(file_path):
     with open(file_path, 'r') as f:
         return yaml.safe_load(f)
+
+# Updates the configuration from environment variables
+def update_config_from_env(config):
+    for conf_key, value in config.items():
+        if conf_key in os.environ:
+            env_val = os.environ[conf_key]
+            if isinstance(config[conf_key], int):  # If the default is an integer
+                config[conf_key] = int(os.environ[conf_key])
+            elif isinstance(value, list) and all(isinstance(item, int) for item in value):
+                config[conf_key] = [int(x) for x in env_val.split(',')]
+            else:
+                config[conf_key] = env_val
+    return config
+  
+# Maps model names to image sizes
+def model_img_size_mapping(model_name):
+    size_mapping = {
+        r'(?i)^ENB0$': 224,  # ENB0
+        r'(?i)^ENB2$': 260,  # ENB2
+        r'(?i)^ENS$': 384,   # ENS
+        r'(?i)^EN[ML]$': 480, # ENM, ENL
+        r'(?i)^ENXL$': 512,  # ENXL
+        r'(?i)^CN[PN]$': 288, # CNP, CNN
+        r'(?i)^CN[TSBL]$': 384, # CNT, CNS, CNB, CNBL
+        r'(?i)^ViT[TBSL]$': 384 # ViTT, ViTS, ViTB, ViTL
+    }
+    # Check the model name against each regex pattern and return the corresponding image size
+    for pattern, size in size_mapping.items():
+        if re.match(pattern, model_name):
+            return size
+    return 384 # Default value of 384px if no match is found
+
+# Sets up the strategy for TensorFlow/JAX training with GPU (single or multiple) or CPU
+def setup_strategy():
+    
+    # Create a context manager that does nothing (for CPU-only training), in the case where no GPU exists
+    class NullStrategy:
+        def scope(self):
+            from contextlib import contextmanager
+            @contextmanager
+            def null_scope():
+                yield
+            return null_scope()
+    
+    gpus = devices()
+    if any('cuda' in str(device).lower() for device in gpus):
+        strategy = distribution.DataParallel(devices=gpus)
+        print(str(len(gpus)) + ' x GPU activated' + '\n')
+    else:
+        strategy = NullStrategy()
+        print('CPU-only training activated' + '\n')
+    return strategy
